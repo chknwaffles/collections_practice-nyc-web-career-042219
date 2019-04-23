abb24cbdc5a1320547c93f0d1141802bb5ce61a1
Won Kim <wonsae.kim@gmail.com>
2019-04-23 14:52:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+require 'pry'
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by {|a| a.length}
+end
+
+def swap_elements(arr)
+  arr.collect do |e|
+    if arr.at(1) == e
+      e = arr.at(2)
+    elsif arr.at(2) == e
+      e = arr.at(1)
+    else
+      e
+    end
+  end
+end
+
+def swap_elements_from_to(arr, origin, dest)
+  arr.collect do |e|
+    if arr.at(origin) == e
+      e == arr.at(dest)
+    elsif arr.at(dest) == e
+      e == arr.at(origin)
+    else
+      e
+    end
+  end
+end
+
+def reverse_array(arr)
+  res = arr.reverse
+end
+
+def kesha_maker(arr)
+  res = []
+  count = 0
+  res = arr.each do |s| 
+    s.chars.map do |c|
+      if count == 2
+        c = '$'
+        #binding.pry
+      end
+      count += 1
+    end.join
+    binding.pry
+  end
+end
